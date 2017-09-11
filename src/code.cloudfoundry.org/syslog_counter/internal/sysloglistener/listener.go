3c22897136be84bd8f84fd416f67a8beb858978c
Johanna Smith <josmith@pivotal.io>
2017-09-11 14:42:25 -0600

    Add syslog counter application
    [#150275113]
    
    Signed-off-by: Brady Love <blove@pivotal.io>

---
+++ src/code.cloudfoundry.org/syslog_counter/internal/sysloglistener/listener.go
@@ -0,0 +1,77 @@
+package sysloglistener
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync/atomic"
+	"time"
+
+	"code.cloudfoundry.org/datadogreporter"
+	"code.cloudfoundry.org/rfc5424"
+)
+
+type SyslogListener struct {
+	logCount int64
+	port     string
+}
+
+func New(port string) *SyslogListener {
+	return &SyslogListener{}
+}
+
+func (sl *SyslogListener) Run() {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%s", sl.port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	log.Print("Listening on " + sl.port)
+
+	for {
+		conn, err := l.Accept()
+		log.Printf("Accepted connection")
+		if err != nil {
+			log.Printf("Error accepting: %s", err)
+			continue
+		}
+
+		go sl.handle(conn)
+	}
+}
+
+func (sl *SyslogListener) handle(conn net.Conn) {
+	defer conn.Close()
+
+	var msg rfc5424.Message
+	for {
+		_, err := msg.ReadFrom(conn)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Printf("ReadFrom err: %s", err)
+			return
+		}
+
+		atomic.AddInt64(&sl.logCount, 1)
+	}
+}
+
+func (sl *SyslogListener) BuildPoints() []datadogreporter.Point {
+	count := atomic.LoadInt64(&sl.logCount)
+
+	return []datadogreporter.Point{
+		{
+			Metric: "capacity_planning.syslog-drain-received",
+			Points: [][]int64{
+				[]int64{time.Now().Unix(), count},
+			},
+			Type: "gauge",
+			Tags: []string{
+				"event_type:logs",
+			},
+		},
+	}
+}
---
+++ src/code.cloudfoundry.org/syslog_counter/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"net/http"
+	"time"
+
+	"code.cloudfoundry.org/datadogreporter"
+	"code.cloudfoundry.org/syslog_counter/internal/sysloglistener"
+)
+
+var (
+	tlsConfig *tls.Config = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	httpClient *http.Client = &http.Client{
+		Timeout: 30 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+)
+
+func main() {
+	port := flag.String("port", "8080", "port to listen on")
+	datadogAPIKey := flag.String("datadog-api-key", "", "Datadog API key.")
+
+	jobName := flag.String("job-name", "", "Name of the bosh job")
+	instanceID := flag.String("instance-id", "", "Bosh job instance ID")
+
+	var missing []string
+	if *port == "" {
+		missing = append(missing, "port")
+	}
+
+	if *datadogAPIKey == "" {
+		missing = append(missing, "datadog-api-key")
+	}
+
+	if *jobName == "" {
+		missing = append(missing, "job-name")
+	}
+
+	if *instanceID == "" {
+		missing = append(missing, "instance-id")
+	}
+
+	lis := sysloglistener.New(*port)
+	go lis.Run()
+
+	reporter := datadogreporter.New(
+		*datadogAPIKey,
+		*jobName,
+		*instanceID,
+		lis,
+		httpClient,
+	)
+	reporter.Run()
+}
